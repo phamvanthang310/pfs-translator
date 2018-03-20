172578564fe0f045e1b9a854fb84e5e26efc95cc
ThangPham <thangpham@kms-technology.com>
2018-03-20 17:40:55 +0700

    Implement read and export file.

--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,66 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment, PureComponent} from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class App extends Component {
+class App extends PureComponent {
+
+  handleFiles = (event) => {
+    const file = event.target.files[0];
+    //TODO: validate file extension *.properties only
+    this.fileName = file.name;
+
+    this.readFileToString(file)
+      .then(data => {
+        console.log(data);
+        this.data = data;
+        console.log(this.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  exportToFile = () => {
+    if (!this.fileName || !this.data) {
+      //TODO: show notification
+      return;
+    }
+
+    const exportData = `data:text/plain;charset=utf-8,${this.data}`;
+    const encodedUri = encodeURI(exportData);
+    const download = document.createElement('a');
+
+    download.setAttribute('href', encodedUri);
+    download.setAttribute('download', this.fileName);
+
+    download.click();
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.fileName = '';
+    this.data = '';
+  }
+
+  readFileToString(fileBlob) {
+    if (!fileBlob) return;
+
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = e => {
+        resolve(e.target.result);
+      };
+      reader.onerror = error => {
+        reject(error);
+      };
+
+      reader.readAsText(fileBlob, 'utf8');
+    });
+  }
+
   render() {
     return (
       <Fragment>
@@ -19,6 +76,8 @@ class App extends Component {
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
+          <input type="file" id="input" onChange={this.handleFiles} accept=".properties"/>
+          <RaisedButton label="Export" primary={true} onClick={this.exportToFile}/>
         </div>
       </Fragment>
     );
