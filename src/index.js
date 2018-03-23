78e4028d6af86083f96d4ed4d2f210e023edd498
ThangPham <thangpham@kms-technology.com>
2018-03-23 11:41:53 +0700

    Fix build failed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './container/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <App/>
-,  document.getElementById('root'));
+  <App/>
+  , document.getElementById('root'));
 registerServiceWorker();
